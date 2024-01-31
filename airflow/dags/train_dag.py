11480ede9edd76b6b61e00d462ff8e8394604370
Максим Фомин <maksimfomin@Maksims-MacBook-Pro.local>
2024-01-31 14:25:33 +0300

    Fixed aiflow

---
+++ airflow/dags/train_dag.py
@@ -0,0 +1,36 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+import pendulum
+
+
+default_args = {
+    "owner": "geramond",
+    # "start_date": days_ago(1),  # запуск день назад
+    "start_date": pendulum.today('UTC').add(days=-1),  # запуск день назад
+    "retries": 5,  # запуск таска до 5 раз, если ошибка
+    #"retry_delay": datetime.timedelta(minutes=5),  # дельта запуска при повторе 5 минут
+    "task_concurency": 1  # одновременно только 1 таск
+}
+
+params = {
+    'name': 'train',
+    'name_dag': 'train_dag',
+    'schedule': '1 * * * *'
+}
+
+def init_dag(dag, task_id):
+    with dag:
+        t1 = BashOperator(
+            task_id=f"{task_id}",
+            bash_command=f"python3 /Users/maksimfomin/IT/DS_practice/4.CV/Similar_to_actor/{params['name']}.py")
+    return dag
+
+
+dag = DAG(params['name_dag'],
+          schedule=params['schedule'],
+          max_active_runs=1,
+          default_args=default_args
+          )
+
+init_dag(dag, params['name_dag'])
+globals()[params['name_dag']] = dag
