0b119457d5298b6a2d558fd24c509fdb4fc42b83
Максим Фомин <maksimfomin@Maksims-MacBook-Pro.local>
2024-02-02 11:51:12 +0300

    Refactored main: Streamlit, btn_train, btn_predict, btn_mlflow; Refactored predict.py

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import os
 import yaml
 import json
+import random
+
+import numpy as np
 
 import pickle
 from PIL import Image
 
-import src
-import train
-import predict
-
 import mlflow
-
-import numpy as np
-
 from fastapi import FastAPI
+import streamlit as st
 from pydantic import BaseModel
 import uvicorn
 
-import streamlit as st
+import src
+import train
+import predict
 
 
 config_path = os.path.join('/Users/maksimfomin/IT/DS_practice/4.CV/Similar_to_actor/config/params.yaml')
@@ -34,10 +33,6 @@ dict_labels = predict.process_dict_labels(dict_labels)
 app = FastAPI()
 
 
-# class ImageToPredict(BaseModel):
-#     path_load: str
-#     test_image:
-
 @st.cache_data
 @app.post('/get_train')
 def get_train():
@@ -51,9 +46,10 @@ def get_train():
 
     return result
 
+
 @st.cache_data
 @app.post('/get_predict')
-def get_predict():
+def get_predict(image_resize):
     mlflow.set_tracking_uri("http://127.0.0.1:5000")
     model_uri_lr = f"models:/{config['model_lr']}/{config['version_lr']}"
     model = mlflow.sklearn.load_model(model_uri_lr)
@@ -68,22 +64,16 @@ def get_predict():
     return result
 
 
-if __name__=='__main__':
-    uvicorn.run(app, host='127.0.0.1', port=80)
-
+def main():
     st.header('Similar actor/actress')
-
     img_file_buffer = st.camera_input("Take a picture")
 
     if img_file_buffer is not None:
-        file_img = img_file_buffer.getvalue()
-        st.write(type(file_img))
+        st.image(img_file_buffer)
+        file_img = Image.open(img_file_buffer)
     else:
         file_img = Image.open(path_load)
     image_resize = np.array(src.resize_images(file_img, size_new=SIZE))
-
-    result = get_predict()
-    st.write(result)
 
     st.markdown(
         """
@@ -92,7 +82,47 @@ if __name__=='__main__':
         """
     )
 
-    button = st.button("Predict")
-    if button:
-        result = get_predict()
-        st.write(f'{result}')
+    button_train = st.button("Train")
+    if button_train:
+        result_train = get_train()
+        st.success(f'{result_train}')
+
+    button_predict = st.button("Predict")
+    if button_predict:
+        result_predict = get_predict(image_resize)
+
+        dir = f"data/raw/{result_predict['predict_labels']}/"
+        path_sample = dir + random.choice(os.listdir(dir))
+
+        image_actress_sample = Image.open(path_sample)
+        st.success(f'{result_predict}')
+        if img_file_buffer:
+            st.image([img_file_buffer, image_actress_sample])
+
+    button_mlflow = st.button("MLFlow")
+    if button_mlflow:
+        mlflow_cmd = "mlflow server --host localhost --port 5000 --backend-store-uri sqlite:///mlflow/mlflow.db --default-artifact-root mlflow"
+        os.system(f"{mlflow_cmd}")
+
+    # button_fastapi = st.button("FastAPI")
+    # if button_fastapi:
+    #     fastapi_cmd = "python3 -m uvicorn main:app --host=127.0.0.1 --port 8000 --reload"
+    #     os.system(f"{fastapi_cmd}")
+
+    # button_airflow = st.button("Airflow")
+    # if button_airflow:
+    #     airflow_cmd = "export AIRFLOW_HOME=~/IT/DS_practice/4.CV/1.Similar_to_actor/airflow"
+    #     os.system(f"{airflow_cmd}")
+    #     airflow_cmd = "airflow webserver -p 8080"
+    #     os.system(f"{airflow_cmd}")
+    #     airflow_cmd = "airflow scheduler"
+    #     os.system(f"{airflow_cmd}")
+
+    # button_docker = st.button("Docker")
+    # if button_docker:
+    #     docker_cmd = "docker build -t similar_to_actor:latest ."
+    #     os.system(f"{docker_cmd}")
+
+
+if __name__=='__main__':
+    main()
